255278d37126ed09f14daa2b77371c225532b284
srikanth <srikanthreddy2510@gmail.com>
2025-11-05 16:35:47 +0530

    Exceptions

---
+++ helper.py
@@ -0,0 +1,18 @@
+# Module example
+
+def calcuCircumference(radius: float) -> float:
+    """
+    calcilates the circumference based on the radius
+    """
+    return 2 * (22/7) * radius
+
+def calcAreaOfCircle(radius=None, diameter = None):
+    """
+    returns area of circle based on radius or diameter
+    """
+    pi = 22/7
+    if radius is not None:
+        return pi * radius ** 2
+    elif diameter is not None:
+        return pi * (diameter/2) ** 2
+    
